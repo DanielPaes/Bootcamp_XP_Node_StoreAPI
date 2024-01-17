cb06b29a79198d71122e603847fb693bae437666
DanielPaes <daniel.p.aes@hotmail.com>
2024-01-17 16:07:52 -0300

    Alterando endereço BD.

--- repositories/db.js
+++ repositories/db.js
@@ -1,10 +1,13 @@
 import Sequelize from "sequelize";
 
-const sequelize = new Sequelize("endereço do BD", {
-  dialect: "postgres",
-  define: {
-    timestamps: false,
-  },
-});
+const sequelize = new Sequelize(
+  "postgres://ivkxyuvi:zTnwLvXBGpEAq2i8kdR0pTaunx926o5V@castor.db.elephantsql.com/ivkxyuvi",
+  {
+    dialect: "postgres",
+    define: {
+      timestamps: false,
+    },
+  }
+);
 
 export default sequelize;
