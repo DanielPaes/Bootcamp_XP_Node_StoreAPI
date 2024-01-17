34bac8e85abfc9cddc81506d94dd86257cc4bf13
DanielPaes <daniel.p.aes@hotmail.com>
2024-01-17 16:13:33 -0300

    BD

--- repositories/db.js
+++ repositories/db.js
@@ -1,13 +1,10 @@
 import Sequelize from "sequelize";
 
-const sequelize = new Sequelize(
-  "postgres://ivkxyuvi:zTnwLvXBGpEAq2i8kdR0pTaunx926o5V@castor.db.elephantsql.com/ivkxyuvi",
-  {
-    dialect: "postgres",
-    define: {
-      timestamps: false,
-    },
-  }
-);
+const sequelize = new Sequelize("Endereço BD", {
+  dialect: "postgres",
+  define: {
+    timestamps: false,
+  },
+});
 
 export default sequelize;
